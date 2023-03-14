6c43682e3993b17dca7a94302fbf4f288f0f23aa
romuloabbiati <rhabbiati@hotmail.com>
2023-03-14 19:14:19 -0300

    Adding JSTL and a button

---
+++ dog-breed-head-first/src/com/example/ListenerTester.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ListenerTester extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+		throws IOException, ServletException {
+		
+		response.setContentType("html/text");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("text context attributes set by listener<br>");
+		
+		out.println("<br>");
+		
+		Dog dog = (Dog) getServletContext().getAttribute("dog");
+		
+		out.println("Dog's breed is: " + dog.getBreed());
+		
+	}
+	
+
+}
